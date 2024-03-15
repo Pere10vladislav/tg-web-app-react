4b5b7110e47324b2fa00971c2220edab8bf0245b
Pere10vladislav <vipereverzev123@gmail.com>
2024-03-15 16:52:16 +0200

    asdasd

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -52,7 +52,7 @@ export default function Form() {
     return (
         (!showWallet ? 
         <div className="form">    
-            <h3>Введите ваши данные</h3>{tg.initDataUnsafe.chat}
+            <h3>Введите ваши данные</h3>
             <input type="text" placeholder={"Имя"} value={rgUser.user} onChange={onchangeName}/>
             <input type="text" placeholder={"Email"} value={rgUser.email} onChange={onchangeEmail}/>
             <input type="text" placeholder={"Trc-20"} value={rgUser.wallet} onChange={onchangeWallet}/>
